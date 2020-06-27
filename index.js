fabbe1b757b8c0385f9d46949329911eef7379ec
Sho <shoishikawasocial@gmail.com>
2020-06-28 00:36:53 +0900

    slideup-bar, cloneNode , Deletion

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
 
-function orderSound(){
-  //let orderButton = document.getElementById('orderButton');
-
-  let sound = new Audio('./pinpon.mp3')
-
-    sound.play();
-
-}
-
 function slideup(){
   let bar = document.getElementById('bar');
 
-  if (bar.style.transform == "translateY(-25%)") {
-    bar.style.transform = "translateY(25%)";
-  }else if(bar.style.transform != "translateY(-25%)"){
-    bar.style.transform = "translateY(-25%)";
+  if (bar.style.transform == "translateY(80%)") {
+    bar.style.transform = "translateY(0)";
+  }else if(bar.style.transform != "translateY(0)"){
+    bar.style.transform = "translateY(80%)";
   }
 }
 
-function ATC(e){
-  console.log("atf");
+function tabSelection(x){
+  let tab = document.getElementsByClassName(this.className);
+  switch (tab) {
+    case x==0:
+    console.log('0');
+      break;
+    case x==1:
+    console.log(1);
+      break;
+    default:
+
+  }
+}
+
+function atc(button){
+  console.log(button.className)
+
+  let item_cloned = button.cloneNode(true);
+  item_cloned.removeAttribute("onclick")
+  let bar = document.getElementById('bar');
+  let sumTxt = document.getElementById('sum');
+  let addedItems = document.getElementById('addedItems');
+  let newDel = document.createElement("button");
+  newDel.className = "deleteFromCartButton";
+  //bar.insertBefore(item_cloned,sumTxt);
+  item_cloned.appendChild(newDel);
+  addedItems.appendChild(item_cloned);
+
+  newDel.addEventListener("click",function(){
+
+    this.closest("div").remove();
+
+  },false);
+
+  //<button class = "addToCartButton"type="button" name="menu-1" onclick="atc(3);"></button>
+  if(addedItems.childElementCount >= 1){
+    let newIcon = document.getElementById('newIcon');
+    newIcon.style.visibility = "visible";
+    }
+
 }
